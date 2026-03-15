2aa84957a6714d284699633f7878d3276eb0977a
Diego <diego.aparicio@etu.unistra.fr>
2026-03-15 01:01:23 +0100

    Adding the fragment to MainActivity

--- app/src/main/java/p42/androidbooksclient/MainActivity.java
+++ app/src/main/java/p42/androidbooksclient/MainActivity.java
@@ -7,18 +7,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LibraryViewModel viewModel;
+
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        initData();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        if (savedInstanceState == null) {
+            fragment_load();
+        }
+    }
+
+    private void initData() {
+        viewModel = new ViewModelProvider(this).get(LibraryViewModel.class);
+
+        String jsonString;
+
+        InputStream inputStream = this.getResources().openRawResource(R.raw.library);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while(true) {
+            try {
+                if ((line = reader.readLine()) == null) break;
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            sb.append( line );
+            sb.append( '\n' );
+        }
+        jsonString = sb.toString();
+
+        viewModel.loadJson(jsonString);
+    }
+
+    private void fragment_load() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView, new BookListFragment())
+                .commit();
     }
 }
