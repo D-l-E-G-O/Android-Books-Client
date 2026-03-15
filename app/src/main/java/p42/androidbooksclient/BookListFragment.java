a7374f17d822bf22b1b515c161d1cae608090432
Diego <diego.aparicio@etu.unistra.fr>
2026-03-15 01:00:48 +0100

    BookListFragment

---
+++ app/src/main/java/p42/androidbooksclient/BookListFragment.java
@@ -0,0 +1,71 @@
+package p42.androidbooksclient;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+
+public class BookListFragment extends Fragment {
+
+    private LibraryViewModel viewModel;
+    private BookAdapter adapter;
+
+    public BookListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_book_list, container, false);
+        ui_init(view);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        observeData();
+    }
+
+    private void ui_init(final View view) {
+        final RecyclerView recyclerView = view.findViewById(R.id.recyclerBooks);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new BookAdapter(new OnBookClickListener() {
+            @Override
+            public void handleClick(final Book book) {
+                // Temporary feedback until the detail fragment is created
+                Toast.makeText(getContext(), "Clicked: " + book.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        recyclerView.setAdapter(adapter);
+
+        final FloatingActionButton fab = view.findViewById(R.id.fabBookAdd);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Toast.makeText(getContext(), "Add Book Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void observeData() {
+        viewModel = new ViewModelProvider(requireActivity()).get(LibraryViewModel.class);
+
+        viewModel.observeBooks().observe(getViewLifecycleOwner(),
+                books -> adapter.updateBooks(new ArrayList<>(books)));
+    }
+}
