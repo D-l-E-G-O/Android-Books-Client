d71a35aa65eec2508d46f5f20bcb74bd93e67f9d
Diego <diego.aparicio@etu.unistra.fr>
2026-04-03 15:57:26 +0200

    Cleanup

--- app/src/main/java/p42/androidbooksclient/BookListFragment.java
+++ app/src/main/java/p42/androidbooksclient/BookListFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
